88c18e1a2fdbb4f38387e1692c73947c89598dae
CodyNeeraj <neerajkumar332424@gmail.com>
2021-04-01 19:33:56 +0530

    Implemented UI of start server screen

--- src/messagecryptor/MessageCryptor.java
+++ src/messagecryptor/MessageCryptor.java
@@ -8,7 +8,7 @@ public class MessageCryptor
    
     public static void main (String[] args)
     {
-        // TODO code application logic here
+        new serverStartFrame().setVisible(true);
     }
     
 }
